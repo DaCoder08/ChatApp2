c6eaacf223bacc2cc790ed9fb7950b03f958694e
DaCoder08 <40702668+DaCoder08@users.noreply.github.com>
2019-06-09 20:31:09 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 var app = require('express')();
-const admin = require('firebase-admin');
-const serviceAccount = require('./chatapp2-78dd3-firebase-adminsdk-mic7b-544b2886aa.json');
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-var db = admin.firestore();
 const express = require('express');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Bob:C00lD%40ta@cluster0-fguxs.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
 server.listen(3000);
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/HomePage.html');
+    res.sendFile(__dirname + '/public/HomePage.html');
 });
 
 //Web Sockets
@@ -20,12 +18,31 @@ app.get('/', function (req, res) {
 var newUser;
 
 io.on('connection', function (socket) {
-  socket.on("data", function (data) {
-      newUser = data;
-      var docRef = db.collection('users').doc(newUser.uname);
-
-      docRef.set(newUser)
-  })
+    socket.on("newUser", function (data) {
+        newUser = data;
+        console.log(newUser)
+        client.connect(err => {
+            const db = client.db("MongoTry")
+            db.collection("users").insertOne(newUser)
+        });
+    })
+    socket.on("login", function (data){
+        client.connect(err => {
+            const db = client.db("MongoTry")
+            db.collection("users").find(data).toArray(function(err, result){
+                if(result.length == 0){
+                    socket.emit("loginCheck", false)
+                }
+                if(result.length != 0){
+                    socket.emit("loginCheck", true)
+                }
+            });
+        });
+    })
 });
 
 app.use(express.static('public'))
+
+
+
+
